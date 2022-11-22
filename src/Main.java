0e0aa7c024206b8367d9b4a27ae11cf971241975
CARLOS <carlos@example.com>
2022-11-22 20:30:20 +0100

    ok

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,55 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int width = 15;
-        int height = 15;
+        int preguntas = scanner.nextInt();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (i+j==((width+height)/2)-1) {
-                    System.out.print("  O  ");
-                } if (i==j) {
-                    System.out.print("  O  ");
-                } else {
-                    System.out.print("  .  ");
-                }
-            }
-            System.out.println();
+        float[] valor = new float[preguntas];
+
+        for (int i = 0; i < preguntas; i++) {
+            valor[i] = scanner.nextInt();
         }
+
+
+        int alumnos = scanner.nextInt();
+
+        float[][] notas = new float[alumnos][preguntas];
+        for (int i = 0; i < alumnos; i++) {
+            for (int j = 0; j < preguntas; j++) {
+                notas[i][j] = scanner.nextInt();
+            }
+        }
+
+
+        float[][] puntuacion = new float[alumnos][preguntas];
+        for (int i = 0; i < alumnos; i++) {
+            for (int j = 0; j < preguntas; j++) {
+                puntuacion[i][j] = notas[i][j] / valor[j];
+            }
+        }
+
+        float[] totales = new float[alumnos];
+        for (int i = 0; i < alumnos; i++) {
+            for (int j = 0; j < preguntas; j++) {
+                totales[i] += puntuacion[i][j];
+            }
+        }
+
+        float max = Integer.MIN_VALUE;
+        for (int i = 0; i < alumnos; i++) {
+            if (totales[i]>max){
+                max = totales[i];
+            }
+        }
+        System.out.println(max);
+
+
+
+
+
+
+
+
+
+
     }
 }
