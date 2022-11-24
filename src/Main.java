0c45ad43dea4aa126be47944fe6e1f2c3386399a
CARLOS <carlos@example.com>
2022-11-24 20:44:13 +0100

    12 45

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,96 @@
 import javax.swing.*;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int tamano = 10; //numero par
 
-        int preguntas = scanner.nextInt();
+        String[] emojis = {"🐙", "🐧", "🐢", "🐊"
+                , "🦖", "🦕", "🦂", "🦄"
+                , "🐿", "🐮", "🐭", "🐫"
+                , "🐯", "🦇", "🐏", "🐒"
+                , "🦛", "🐂", "🦥", "🐻"
+                , "🐺", "🐼", "💑", "🧟"
+                , "🎅", "🫀", "🧠", "💩"
+                , "👻", "👿", "🥺", "🥵"
+                , "😛", "🤑", "🤣", "😉"
+                , "♿", "🚫", "🚸", "🔝"
+                , "🛐", "☮", "🔰", "🔱"
+                , "⚜", "🔟", "⚫", "🟤"
+                , "🟥", "🚩"};
 
-        float[] valor = new float[preguntas];
+        int cuantos = tamano * tamano / 2;
 
-        for (int i = 0; i < preguntas; i++) {
-            valor[i] = scanner.nextInt();
-        }
+        String[][] tablero = new String[tamano][tamano];
 
+        boolean[][] acertada = new boolean[tamano][tamano];
 
-        int alumnos = scanner.nextInt();
+        for (int i = 0; i < cuantos; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (; ; ) {
+                    int f1 = random.nextInt(tamano);
+                    int c1 = random.nextInt(tamano);
 
-        float[][] notas = new float[alumnos][preguntas];
-        for (int i = 0; i < alumnos; i++) {
-            for (int j = 0; j < preguntas; j++) {
-                notas[i][j] = scanner.nextInt();
+                    if (tablero[f1][c1] == null) {
+                        tablero[f1][c1] = emojis[i];
+                        break;
+                    }
+                }
             }
         }
 
 
-        float[][] puntuacion = new float[alumnos][preguntas];
-        for (int i = 0; i < alumnos; i++) {
-            for (int j = 0; j < preguntas; j++) {
-                puntuacion[i][j] = notas[i][j] / valor[j];
+//        for (int i = 0; i < tamano; i++) {
+//            for (int j = 0; j < tamano; j++) {
+//                System.out.print(tablero[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 0; i < tamano; i++) {
+            for (int j = 0; j < tamano; j++) {
+                System.out.print("🌊");
             }
+            System.out.println();
         }
 
-        float[] totales = new float[alumnos];
-        for (int i = 0; i < alumnos; i++) {
-            for (int j = 0; j < preguntas; j++) {
-                totales[i] += puntuacion[i][j];
+        for (; ; ) {
+
+            //if (completado){break;}
+            int f1 = scanner.nextInt(), c1 = scanner.nextInt();
+            int f2 = scanner.nextInt(), c2 = scanner.nextInt();
+
+            if (tablero[f1][c1].equals(tablero[f2][c2])) {
+                acertada[f1][c1] = true;
+                acertada[f2][c2] = true;
             }
-        }
 
-        float max = Integer.MIN_VALUE;
-        for (int i = 0; i < alumnos; i++) {
-            if (totales[i]>max){
-                max = totales[i];
+//            System.out.print("\033[H\033[2J");
+//            System.out.flush();
+
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
+            for (int i = 0; i < tamano; i++) {
+                for (int j = 0; j < tamano; j++) {
+                    if (acertada[i][j]) {
+                        System.out.print(tablero[i][j]);
+                    } else if (i == f1 && j == c1) {
+                        System.out.print(tablero[i][j]);
+                    } else if (i == f2 && j == c2) {
+                        System.out.print(tablero[i][j]);
+                    } else {
+                        System.out.print("\uD83C\uDF0A");
+                    }
+                }
+                System.out.println();
             }
+
+
         }
-        System.out.println(max);
-
-
-
-
-
-
-
-
-
 
     }
 }
